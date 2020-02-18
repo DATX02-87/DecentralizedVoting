ee670e1e1428c681dd3772f305c7aa6249500a5b
Oskar Wallgren <oskwall@student.chalmers.se>
2020-02-18 09:50:55 +0100

    implemented interface Engine

---
+++ consensus-engine/src/main/java/se/chalmers/datx02/Engine.java
@@ -0,0 +1,38 @@
+package se.chalmers.datx02;
+
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+
+public interface Engine {
+
+    /**
+     * Called after the engine is initialized, when a connection to the
+     *         validator has been established. Notifications from the
+     *         validator are sent along UPDATES. SERVICE is used to send
+     *         requests to the validator.
+     */
+    void start(BlockingQueue<Object> updates, Service service, StartupInfo startupInfo);
+
+    /**
+     * Called before the engine is dropped
+     * in order to give the engine a chance to notify peers and clean up.
+     */
+    void stop();
+
+    /**
+     * Get the version of this engine.
+     */
+    String getVersion();
+
+    /**
+     * Get the name of the engine, typically the algorithm being implemented.
+     */
+    String getName();
+
+    /**
+     *  Any additional protocol name/version pairs this engine supports
+     */
+    Map<String, String> additionalProtocol();
+
+}
