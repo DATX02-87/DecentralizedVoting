c7b2b74ea29dd470ecab361ab06bf6d2b4cbc151
vltmn <melkerveltman@gmail.com>
2020-03-01 12:47:20 +0100

    add documentation driver

--- consensus-engine/src/main/java/se/chalmers/datx02/lib/Driver.java
+++ consensus-engine/src/main/java/se/chalmers/datx02/lib/Driver.java
@@ -1,10 +1,17 @@
 package se.chalmers.datx02.lib;
 
+/**
+ * Describes a driver for the consensus engine, handles communication from the validator to the engine
+ */
 public interface Driver {
-
-    void Driver(Engine engine);
-
+    /**
+     * Start the driver with the server running on the specified endpoint
+     * @param endpoint the endpoint to run the server on
+     */
     void start(String endpoint);
 
+    /**
+     * Stop the driver, stopping the consensus engine
+     */
     void stop();
 }
