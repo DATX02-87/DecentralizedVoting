0a9a2e212419cc77b6d90fa2b18170ffc9a1133b
vltmn <melkerveltman@gmail.com>
2020-03-09 11:41:45 +0100

    add endpoint as required option for cmd engine

--- consensus-engine/src/main/java/se/chalmers/datx02/Application.java
+++ consensus-engine/src/main/java/se/chalmers/datx02/Application.java
@@ -6,7 +6,7 @@ import se.chalmers.datx02.lib.ZmqDriver;
 
 @CommandLine.Command(name = "Hyperledger Sawtooth Devmode engine Java", mixinStandardHelpOptions = true, version = "1.0")
 public class Application implements Runnable {
-    @CommandLine.Option(names = {"-e", "--endpoint"}, description = "Validator endpoint")
+    @CommandLine.Option(names = {"-e", "--endpoint"}, description = "Validator endpoint", required = true)
     private String endpoint;
 
 
