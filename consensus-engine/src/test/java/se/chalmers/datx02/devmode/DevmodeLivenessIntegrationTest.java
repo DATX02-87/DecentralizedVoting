130d67f7f3ccc75a5472cbff33eb2acb6f4f6c51
vltmn <melkerveltman@gmail.com>
2020-03-11 21:15:37 +0100

    consensus: cleanup liveness integration test

--- consensus-engine/src/test/java/se/chalmers/datx02/devmode/DevmodeLivenessIntegrationTest.java
+++ consensus-engine/src/test/java/se/chalmers/datx02/devmode/DevmodeLivenessIntegrationTest.java
@@ -1,25 +1,11 @@
 package se.chalmers.datx02.devmode;
 
-import com.google.api.client.http.GenericUrl;
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpRequestFactory;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.GenericJson;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.JsonObjectParser;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.Key;
-import com.palantir.docker.compose.DockerComposeExtension;
-import com.palantir.docker.compose.connection.Container;
-import com.palantir.docker.compose.connection.DockerPort;
-import com.palantir.docker.compose.connection.waiting.SuccessOrFailure;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import se.chalmers.datx02.testutils.Block;
 import se.chalmers.datx02.testutils.Response;
+import se.chalmers.datx02.testutils.SawtoothComposeExtension;
 
 import java.io.IOException;
 import java.util.*;
@@ -30,10 +16,8 @@ import java.util.stream.IntStream;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Tag(value = "integration")
-class DevmodeEngineIntegrationTest {
-    private final static Logger LOGGER = Logger.getLogger(DevmodeEngineIntegrationTest.class.getName());
-    private static HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
-    private static JsonFactory JSON_FACTORY = new JacksonFactory();
+class DevmodeLivenessIntegrationTest {
+    private final static Logger LOGGER = Logger.getLogger(DevmodeLivenessIntegrationTest.class.getName());
     private static final int COMPONENT_PORT = 4004;
     public static final int VALIDATOR_PORT = 8800;
     public static final int CONSENSUS_PORT = 5005;
@@ -44,32 +28,15 @@ class DevmodeEngineIntegrationTest {
     public static final int MIN_TOTAL_BATCHES = 100;
     public static final int REST_API_PORT = 8008;
     private static final List<Integer> INSTANCES = IntStream.range(0, 5).boxed().collect(Collectors.toList());
-    private static HttpRequestFactory requestFactory;
-
-
-    @BeforeAll
-    public static void before() {
-        requestFactory
-                = HTTP_TRANSPORT.createRequestFactory(
-                (HttpRequest request) -> {
-                    request.setParser(new JsonObjectParser(JSON_FACTORY));
-                });
-    }
 
     @RegisterExtension
-    public static DockerComposeExtension docker = DockerComposeExtension.builder()
-            .file("src/test/resources/integration/test_devmode_engine_liveness.yaml")
-            .waitingForServices(
-                    INSTANCES.stream()
-                            .map(i -> Arrays.asList("validator-" + i, "rest-api-" + i, "intkey-tp-" + i, "settings-tp-" + i))
-                            .flatMap(Collection::stream)
-                            .collect(Collectors.toList()),
-                    target -> target.stream()
-                            .map(Container::areAllPortsOpen)
-                            .filter(SuccessOrFailure::failed)
-                            .findAny().orElse(SuccessOrFailure.success()))
-            .saveLogsTo("logs/docker-logs/" + DevmodeEngineIntegrationTest.class.getSimpleName())
-            .build();
+    public static SawtoothComposeExtension sawtoothComposeExtension = new SawtoothComposeExtension(
+            INSTANCES.stream()
+                    .map(i -> Arrays.asList("validator-" + i, "rest-api-" + i, "intkey-tp-" + i, "settings-tp-" + i))
+                    .flatMap(Collection::stream)
+                    .collect(Collectors.toList()),
+            "src/test/resources/integration/test_devmode_engine_liveness.yaml"
+    );
 
     @Test
     public void test() throws InterruptedException {
@@ -97,13 +64,14 @@ class DevmodeEngineIntegrationTest {
                 }
                 logBlock(i, block);
             }
-            Thread.sleep(1000);
+            Thread.sleep(5000);
         }
         List<List<Block>> chains = getChains();
         // test that all nodes are in consensus
         assertTrue(checkConsensus(chains, BLOCKS_TO_CHECK_CONSENSUS));
         // test that an acceptable number of batches has been committed
         assertTrue(checkMinBatches(chains.get(0), MIN_TOTAL_BATCHES));
+        LOGGER.info("Test done, now stopping docker compose");
     }
 
     private boolean checkMinBatches(List<Block> blocks, int minBatches) {
@@ -151,34 +119,18 @@ class DevmodeEngineIntegrationTest {
 
 
     private Block getBlock(int node) throws IOException {
-        String address = buildUri(node, "blocks?count=1");
-        Response response = getRequest(address);
+        String address = sawtoothComposeExtension.buildUri("rest-api-" + node, REST_API_PORT, "blocks?count=1");
+        Response response = sawtoothComposeExtension.getRequest(address, Response.class);
         return response.getData().get(0);
     }
 
     private List<Block> getAllBlocks(int node) {
-        String address = buildUri(node, "blocks");
+        String address = sawtoothComposeExtension.buildUri("rest-api-" + node, REST_API_PORT, "blocks");
         try {
-            return getRequest(address).getData();
+            return sawtoothComposeExtension.getRequest(address, Response.class).getData();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-
-    private static String buildUri(int node, String path) {
-        Container container = docker.containers().container("rest-api-" + node);
-        DockerPort port = container.port(REST_API_PORT);
-        return String.format("http://%s:%s/%s", port.getIp(), port.getExternalPort(), path);
-    }
-
-    private Response getRequest(String uri) throws IOException {
-        return requestFactory.buildGetRequest(new GenericUrl(uri)).execute()
-                .parseAs(Response.class);
-    }
-
-
-
-
-
 
 }
---
+++ consensus-engine/src/test/java/se/chalmers/datx02/testutils/SawtoothComposeExtension.java
@@ -0,0 +1,78 @@
+package se.chalmers.datx02.testutils;
+
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.JsonObjectParser;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.palantir.docker.compose.DockerComposeExtension;
+import com.palantir.docker.compose.connection.Container;
+import com.palantir.docker.compose.connection.DockerPort;
+import com.palantir.docker.compose.connection.waiting.SuccessOrFailure;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.io.IOException;
+import java.util.List;
+
+public class SawtoothComposeExtension implements BeforeAllCallback, AfterAllCallback {
+
+    private HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+    private JsonFactory JSON_FACTORY = new JacksonFactory();
+    private HttpRequestFactory requestFactory;
+
+    private final List<String> waitForInstances;
+
+    private DockerComposeExtension docker;
+
+    public SawtoothComposeExtension(List<String> waitForInstances, String composeFile) {
+        this.waitForInstances = waitForInstances;
+        docker = DockerComposeExtension.builder()
+                .file(composeFile)
+                .waitingForServices(
+                        waitForInstances,
+                        target -> target.stream()
+                                .map(Container::areAllPortsOpen)
+                                .filter(SuccessOrFailure::failed)
+                                .findAny().orElse(SuccessOrFailure.success()))
+                .build();
+    }
+
+    @Override
+    public void afterAll(ExtensionContext context) throws Exception {
+        docker.afterAll(context);
+    }
+
+    @Override
+    public void beforeAll(ExtensionContext context) throws Exception {
+        docker.beforeAll(context);
+        requestFactory
+                = HTTP_TRANSPORT.createRequestFactory(
+                (HttpRequest request) -> {
+                    request.setParser(new JsonObjectParser(JSON_FACTORY));
+                });
+    }
+
+    public String buildUri(String containerName, int port, String path) {
+        Container container = docker.containers().container(containerName);
+        DockerPort dockerPort = container.port(port);
+        return String.format("http://%s:%s/%s", dockerPort.getIp(), dockerPort.getExternalPort(), path);
+    }
+
+    public String buildUri(String transport, String containerName, int port, String path) {
+        Container container = docker.containers().container(containerName);
+        DockerPort dockerPort = container.port(port);
+        return String.format("%s://%s:%s/%s", transport, dockerPort.getIp(), dockerPort.getExternalPort(), path);
+    }
+
+    public <T> T getRequest(String uri, Class<T> clazz) throws IOException {
+        return requestFactory.buildGetRequest(new GenericUrl(uri)).execute()
+                .parseAs(clazz);
+    }
+
+
+}
