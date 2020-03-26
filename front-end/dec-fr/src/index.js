93ae3c384af6d064d0177f5919c1475144231fbe
OptiGE <optige@gmail.com>
2020-03-26 18:56:52 +0100

    Lade till frontendstruktur

---
+++ front-end/dec-fr/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const name = 'Grupp 81';
+const element = <h1>{name}</h1>;
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
