48de28045871d7b39bf9e2587a471baa5cd3c578
Omar Hindawi <omarhindawi98@gmail.com>
2020-03-30 21:05:56 +0200

    Moved lib files & adaded storage files. Needs impl

--- consensus-engine/src/main/java/se/chalmers/datx02/PBFT/Config.java
+++ consensus-engine/src/main/java/se/chalmers/datx02/PBFT/Config.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.org.apache.xerces.internal.impl.dv.util.HexBin;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import se.chalmers.datx02.PBFT.lib.RetryUntilOk;
+import se.chalmers.datx02.PBFT.lib.timing.RetryUntilOk;
 import se.chalmers.datx02.lib.exceptions.ReceiveErrorException;
 import se.chalmers.datx02.lib.exceptions.UnknownBlockException;
 
@@ -13,7 +13,6 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
--- consensus-engine/src/main/java/se/chalmers/datx02/PBFT/State.java
+++ consensus-engine/src/main/java/se/chalmers/datx02/PBFT/State.java
@@ -2,7 +2,7 @@ package se.chalmers.datx02.PBFT;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import se.chalmers.datx02.PBFT.lib.Timeout;
+import se.chalmers.datx02.PBFT.lib.timing.Timeout;
 import se.chalmers.datx02.PBFT.lib.exceptions.InternalError;
 
 import java.time.Duration;
