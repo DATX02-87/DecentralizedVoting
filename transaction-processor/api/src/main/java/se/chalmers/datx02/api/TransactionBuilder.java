5eae513e3e5bdc9af50770252aa3f400bbd112e6
vltmn <melkerveltman@gmail.com>
2020-04-05 18:05:03 +0200

    comment out unused code

--- transaction-processor/api/src/main/java/se/chalmers/datx02/api/TransactionBuilder.java
+++ transaction-processor/api/src/main/java/se/chalmers/datx02/api/TransactionBuilder.java
@@ -1,27 +1,17 @@
 package se.chalmers.datx02.api;
 
-import sawtooth.sdk.protobuf.BatchList;
-import sawtooth.sdk.protobuf.BatchHeader;
-import sawtooth.sdk.protobuf.Transaction;
-import sawtooth.sdk.signing.PrivateKey;
-import sawtooth.sdk.signing.Secp256k1Context;
-import sawtooth.sdk.signing.Signer;
-import co.nstant.in.cbor.CborBuilder;
-import co.nstant.in.cbor.CborEncoder;
-import co.nstant.in.cbor.CborException;
-
 public class TransactionBuilder {
 
-    public enum Action{
-        AddCandidate,
-        getVoteCount,
-        Vote
-    }
-    private Secp256k1Context context = new Secp256k1Context();
-    private PrivateKey privateKey = context.newRandomPrivateKey();
-    private Signer signer = new Signer(context, privateKey);
-
-    public void buildTransaction(Transaction t) {
-        
-    }
+//    public enum Action{
+//        AddCandidate,
+//        getVoteCount,
+//        Vote
+//    }
+//    private Secp256k1Context context = new Secp256k1Context();
+//    private PrivateKey privateKey = context.newRandomPrivateKey();
+//    private Signer signer = new Signer(context, privateKey);
+//
+//    public void buildTransaction(Transaction t) {
+//
+//    }
 }
