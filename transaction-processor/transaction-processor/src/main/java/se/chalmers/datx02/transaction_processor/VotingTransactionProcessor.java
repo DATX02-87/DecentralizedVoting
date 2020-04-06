fc666128aa720ccb784232cc5177ce398f72c4d5
vltmn <melkerveltman@gmail.com>
2020-04-06 17:21:54 +0200

    transaction-processor: join the transactionprocessor thread if it stops

--- transaction-processor/transaction-processor/src/main/java/se/chalmers/datx02/transaction_processor/VotingTransactionProcessor.java
+++ transaction-processor/transaction-processor/src/main/java/se/chalmers/datx02/transaction_processor/VotingTransactionProcessor.java
@@ -5,12 +5,13 @@ import sawtooth.sdk.processor.TransactionProcessor;
 
 public class VotingTransactionProcessor {
   
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
   	// Connect to validator
     TransactionProcessor transactionProcessor = new TransactionProcessor(args[0]);
     // Register handler with the validator
     transactionProcessor.addHandler(new VotingHandler(args[1]));
     Thread thread = new Thread(transactionProcessor);
     thread.start();
+    thread.join();
   }
 }
