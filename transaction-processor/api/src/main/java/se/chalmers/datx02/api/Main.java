72ab06ab3baef9fe716c0e8966b1ce326d5e935e
vltmn <melkerveltman@gmail.com>
2020-04-06 15:15:27 +0200

    implement application startup for api

--- transaction-processor/api/src/main/java/se/chalmers/datx02/api/Main.java
+++ transaction-processor/api/src/main/java/se/chalmers/datx02/api/Main.java
@@ -3,12 +3,14 @@ package se.chalmers.datx02.api;
 import org.rapidoid.setup.App;
 import sawtooth.sdk.signing.Secp256k1Context;
 import sawtooth.sdk.signing.Signer;
+import se.chalmers.datx02.model.Adressing;
 
 public class Main {
     public static void main(String[] args) {
         Secp256k1Context context = new Secp256k1Context();
         Signer s = new Signer(context, context.newRandomPrivateKey());
-        ValidatorService.init("", "", s);
-        App.bootstrap(args);
+        String stateAddress = Adressing.makeMasterAddress(args[1]);
+        ValidatorService.init(args[0], stateAddress, s);
+        App.bootstrap(new String[]{});
     }
 }
