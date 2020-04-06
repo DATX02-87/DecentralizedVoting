e1291620288100cb4703b899a082b84f23803941
vltmn <melkerveltman@gmail.com>
2020-04-06 09:13:19 +0200

    rename variable in adressing util

--- transaction-processor/model/src/main/java/se/chalmers/datx02/model/Adressing.java
+++ transaction-processor/model/src/main/java/se/chalmers/datx02/model/Adressing.java
@@ -8,9 +8,9 @@ public class Adressing {
     public static final String FAMILY_NAME = "dcvs02";
     public static final String FAMILY_VERSION = "1.0";
 
-    public static String makeMasterAddress(String masterName) {
+    public static String makeMasterAddress(String masterStateName) {
         String nameSpace = Utils.hash512(FAMILY_NAME.getBytes(StandardCharsets.UTF_8)).substring(0, 6);
-        String hashedName = Utils.hash512(masterName.getBytes(StandardCharsets.UTF_8));
+        String hashedName = Utils.hash512(masterStateName.getBytes(StandardCharsets.UTF_8));
         return nameSpace + hashedName.substring(0, 64);
     }
 }
