805bd316bd52087358ed4a1f1b530dcaa14f9ce8
tomt <tomt@student.chalmers.se>
2020-04-06 11:25:24 +0200

    Refactored ValidatorCommunicator to Singleton-class

--- transaction-processor/api/src/main/java/se/chalmers/datx02/api/RestController.java
+++ transaction-processor/api/src/main/java/se/chalmers/datx02/api/RestController.java
@@ -4,14 +4,17 @@ import com.google.protobuf.ByteString;
 import org.rapidoid.annotation.Controller;
 import org.rapidoid.annotation.GET;
 import org.rapidoid.annotation.POST;
+import org.rapidoid.annotation.Param;
 import sawtooth.sdk.protobuf.Transaction;
 import se.chalmers.datx02.api.model.api.PostTransactionRequest;
 import se.chalmers.datx02.api.model.api.PostTransactionResponse;
 import se.chalmers.datx02.model.DataUtil;
 import se.chalmers.datx02.model.TransactionPayload;
+import se.chalmers.datx02.model.state.Election;
 import se.chalmers.datx02.model.state.GlobalState;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Controller("/rest")
 public class RestController {
@@ -32,6 +35,18 @@ public class RestController {
     @GET("/state")
     public GlobalState getGlobalState() throws IOException {
         return validatorService.getState();
+    }
+
+    @GET("/elections")
+    public Map<String, Election> getEligibleElections(@Param("key") String key) throws IOException, NullPointerException{
+        Map <String, Election> map = null;
+        GlobalState state = validatorService.getState();
+
+        for(Map.Entry<String, Election> e : state.getElections().entrySet()){
+            if (e.getKey().equals(key))
+                map.put(key, e.getValue());
+        }
+        return map;
     }
 
 
