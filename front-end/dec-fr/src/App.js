cff28e0a8ff324353c423c2fbaafa9d20afdb829
vltmn <melkerveltman@gmail.com>
2020-04-19 18:19:54 +0200

    frontend: use sass styling, remove unused css

--- front-end/dec-fr/src/App.js
+++ front-end/dec-fr/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
 import Home from './components/pages/Home';
 import MyNavbar from './components/layout/MyNavbar';
 import Votations from './components/votations/Votations';
@@ -15,15 +13,15 @@ const App = () => {
     <VoteState>
       <KeyState>
         <Router>
-          <div className='App' style={{ backgroundColor: '#F0F0F7' }}>
+          <div className='app'>
             <MyNavbar />
-            <div className='container'>
+            <Container>
               <Switch>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/votations' component={Votations} />
                 <Route exact path='/votation/:name' component={Votation} />
               </Switch>
-            </div>
+            </Container>
           </div>
         </Router>
       </KeyState>
