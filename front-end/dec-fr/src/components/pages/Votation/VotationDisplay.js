cae60e325fb60f54091898c31e2208611114fe39
vltmn <melkerveltman@gmail.com>
2020-04-19 22:14:58 +0200

    frontend: refactor votation page to presentation and container component for separation of concern

---
+++ front-end/dec-fr/src/components/pages/Votation/VotationDisplay.js
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { Link } from 'react-router-dom';
+
+const SelectCandidateForm = ({candidates, selectedCandidate, onChange, onSubmit, castingVote}) => (
+    <Form onSubmit={onSubmit}>
+        <Form.Group controlId="selectCandidateForm">
+            <Form.Label>Please pick an option below</Form.Label>
+            <Form.Control as="select" value={selectedCandidate} custom onChange={onChange} disabled={castingVote}>
+                <option></option>
+                {candidates.map((candidate) => (
+                    <option key={candidate}>{candidate}</option>
+                ))}
+            </Form.Control>
+        </Form.Group>
+        <ButtonGroup>
+            <Button variant='primary' type='submit' disabled={selectedCandidate === '' || !selectedCandidate || castingVote}>
+                Cast vote
+            </Button>
+            <Link to='/' className='btn btn-light'>Back</Link>
+        </ButtonGroup>
+    </Form>
+);
+const VotationDisplay = ({name, active, hasVoted, candidates, castingVote, onVote}) => {
+    const [selectedCandidate, setSelectedCandidate] = useState(undefined);
+    const onChange = (evt) => {
+        setSelectedCandidate(evt.target.value);
+    }
+    const onSubmit = (evt) => {
+        evt.preventDefault();
+        if (!selectedCandidate) {
+            throw new Error('A candidate should be selected');
+        }
+        onVote(selectedCandidate);
+    }
+    return (
+    <Card>
+        <Card.Body>
+            <Card.Title>{name}</Card.Title>
+            {active ? (
+                <Card.Subtitle className="text-muted">Currently active</Card.Subtitle>
+            ) : (
+                <Card.Subtitle className="text-muted">Ended</Card.Subtitle>
+            )}
+            {hasVoted ? (
+            <>
+                {active ? (
+                <Card.Text>
+                    You have successfully voted in this votation, results will be posted after the end of this votation.
+                </Card.Text>
+                ) : (
+                    <p>SHOW STATS HERE</p>
+                )}
+                
+                <Link to='/' className='btn btn-light'>Back</Link>
+            </>
+            ) : (
+            <SelectCandidateForm
+                candidates={candidates}
+                selectedCandidate={selectedCandidate}
+                onChange={onChange}
+                onSubmit={onSubmit}
+                castingVote={castingVote}
+            />
+            )}
+        </Card.Body>
+    </Card>
+    );
+};
+
+VotationDisplay.propTypes = {
+    name: PropTypes.string.isRequired,
+    active: PropTypes.bool.isRequired,
+    hasVoted: PropTypes.bool.isRequired,
+    candidates: PropTypes.array.isRequired,
+    castingVote: PropTypes.bool.isRequired,
+    onVote: PropTypes.func.isRequired
+};
+
+export default VotationDisplay;
--- front-end/dec-fr/src/components/pages/Votation/index.js
+++ front-end/dec-fr/src/components/pages/Votation/index.js
@@ -1,88 +1,37 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import { Row, ButtonGroup, Spinner } from 'react-bootstrap';
+import React, { useContext, useState, useEffect, useCallback } from 'react';
+import { Spinner } from 'react-bootstrap';
 
-import { castVote, getElection } from '../../../services/api';
-import KeyContext from '../../context/key/keyContext';
+import { castVote, getElection } from 'services/api';
+import KeyContext from 'components/context/key/keyContext';
+import VotationDisplay from './VotationDisplay';
 
 const Votation = ({ match }) => {
   const { key } = useContext(KeyContext);
 
-  const [selectedCandidate, setSelectedCandidate] = useState(undefined);
-  const onChange = (e) => setSelectedCandidate(e.target.value);
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (!selectedCandidate) {
-      throw new Error('A candidate should be selected')
-    }
-    // TODO 
-    castVote(key, votation.name, selectedCandidate);
-    return true;
-  };
-
   const [votation, setVotation] = useState(undefined);
   const votationName = match.params.name;
-  useEffect(() => {
-    getElection(votationName, key).then(votation => {
-      setVotation(votation);
-    });
+  const getElectionData = useCallback(async () => {
+    const votation = await getElection(votationName, key);
+    setVotation(votation);
   }, [votationName, key]);
 
+  useEffect(() => {
+    getElectionData();
+  }, [votationName, key, getElectionData]);
+
+  const [castingVote, setCastingVote] = useState(false);
+  const onVote = candidate => {
+    setCastingVote(true);
+    castVote(key, votation.name, candidate)
+      .then(() => getElectionData())
+      .then(() => setCastingVote(false));
+  }
   if (!votation) {
     return <Spinner />;
   }
-  const { name, active, hasVoted, candidates } = votation;
+  
   return (
-    <Card as='div' className='card grid-2'>
-      <Row>
-        <Col as='div' className='all-center'>
-          <Card.Title>Election name: {name}</Card.Title>
-          <Card.Body>
-            <Card.Text>Active: {active.toString()}</Card.Text>
-            <Card.Text>Voted: {hasVoted.toString()}</Card.Text>
-          </Card.Body>
-        </Col>
-        <Col as='div' className='all-center'>
-          <Form onSubmit={onSubmit}>
-            <Row>
-              <Col>
-                <fieldset>
-                  <Form.Group controlId='voteForm'>
-                    <Form.Label as='h5'>Candidates</Form.Label>
-                    {candidates.map((candidate) => (
-                      <div key={candidate} className='mb-3'>
-                        <Form.Check
-                          type='radio'
-                          name='voteForm'
-                          id={candidate}
-                          label={candidate}
-                          value={candidate}
-                          onChange={onChange}
-                        />
-                      </div>
-                    ))}
-                  </Form.Group>
-                </fieldset>
-              </Col>
-              <Col>
-                <ButtonGroup>
-                  <Button size='sm' type='submit'>
-                    Submit
-                  </Button>
-                  <Link to='/' className='btn btn-light'>
-                    Back
-                  </Link>
-                </ButtonGroup>
-              </Col>
-            </Row>
-          </Form>
-        </Col>
-      </Row>
-    </Card>
+    <VotationDisplay {...votation} castingVote={castingVote} onVote={onVote}/>
   );
 };
 
