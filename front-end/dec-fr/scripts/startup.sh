84b06e186b47aa9115d22aeafc7c2e6a527b33be
vltmn <melkerveltman@gmail.com>
2020-05-23 14:25:28 +0200

    frontend: add deployment scrpits, update package-lock

---
+++ front-end/dec-fr/scripts/startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -z "$INIT" ]
+then
+    echo "Not running init scripts"
+else
+    echo "Waiting 20 seconds until API has come up"
+    sleep 20
+    node src/init-votes.js
+fi
+npm run-script build
